af56de90c1638ced336d3d1feb23f58726f5b3ad
Martin Averame <maverame@avemnet.com>
2023-11-24 22:28:42 -0300

    6546546546

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,5 @@ import App from './App.js';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText((_, element) => element.textContent === 'learn react')).toBeInTheDocument();
 });
